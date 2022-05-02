b68ff9de350215319c198b53e44dffee8f9d1730
ProfessorSOak <hugsta19@student.hh.se>
2022-05-02 14:24:07 +0200

    Creatin the painting class

--- src/Panel.java
+++ src/Panel.java
@@ -3,6 +3,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 public class Panel {
+
+    int CharXpos;
+    int CharYpos;
+    int[][] field;
+
     public Panel(){
         Level1();
 
@@ -12,7 +17,23 @@ public class Panel {
         JFrame frame = new JFrame();
         GridLayout gird = new GridLayout(9,8);
         frame.setLayout(gird);
-        int[][] a = new int[gird.getRows()][gird.getColumns()];
+        int[][] field = new int[][]{
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,1,1,1,0,0},
+                {0,4,3,2,1,1,0,0},
+                {0,0,0,1,2,4,0,0},
+                {0,4,0,0,2,1,0,0},
+                {0,1,0,1,4,1,0,0},
+                {0,2,1,6,2,2,4,0},
+                {0,1,1,1,4,1,1,0},
+                {0,0,0,0,0,0,0,0}};
+        this.field = field;
+        //Crates Value = 2;
+        //goals value = 4
+        //character value = 3;
+        CharXpos = 2;
+        CharYpos = 2;
+        //tile values = 1;
     }
 
 }
