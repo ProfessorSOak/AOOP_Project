ed30d0e3456b6a6cc16c0ef713f6c00cc0e61290
ProfessorSOak <hugsta19@student.hh.se>
2022-05-23 11:38:03 +0200

    Made the Graphical view update through an observer and changed the pointer copy.

--- src/ObserverCollection.java
+++ src/ObserverCollection.java
@@ -5,19 +5,14 @@ public class ObserverCollection implements BoardObserver{
     @Override
     public void UpdateBoard(int[][] field, String update, File[][] FileBoard) {//method for when we update an observer
 
-        if(update.equals("Game Started")){
-            System.out.println(update + "\nCurrent Board:");
-        }
-
-        else if(update.equals("Reset")){// checks if the board was reset
-            System.out.println("Game " + update + "\nCurrent Board:");
-        }
-
-        else if(update.equals("Cleared Game")){ //check if game was cleared
-            System.out.println(update + "\nFinal Board:");
-        }
-        else { //otherwise, prints out the direction that was pressed
-            System.out.println("Moved: " + update + "\nCurrent Board:");
+        switch (update) {
+            case "Game Started" -> System.out.println(update + "\nCurrent Board:");
+            case "Reset" -> // checks if the board was reset
+                    System.out.println("Game " + update + "\nCurrent Board:");
+            case "Cleared Game" ->  //check if game was cleared
+                    System.out.println(update + "\nFinal Board:");
+            default ->  //otherwise, prints out the direction that was pressed
+                    System.out.println("Moved: " + update + "\nCurrent Board:");
         }
         StringBuilder Board = new StringBuilder();//makes a string of the field to print out how the game looks in a text based fashion
         for(int i = 0; i<9; i++){
